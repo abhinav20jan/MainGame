c53cdafcde12059da37186416f24604271f4b19c
dxaman <niketdx@iitk.ac.in>
2020-05-17 17:55:09 +0530

    Project Board Added

---
+++ Falling Game/Assets/Scripts/cameraMotor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMotor : MonoBehaviour
+{
+    Transform lookAt;
+    Vector3 startOffset;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt=GameObject.player.transform;
+        startOffset=transform.position-lookAt.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=lookAt.position+startOffset;
+        
+    }
+}
